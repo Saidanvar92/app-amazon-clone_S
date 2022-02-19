2f2b803ded69fb061b0755825d7921131ba24da3
Saidanvarxo'ja <94392814+Saidanvar92@users.noreply.github.com>
2022-02-20 03:09:32 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import {Home} from "./pages/home/Home";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/cart" exact component={Cart}/>
+            </Switch>
+        </BrowserRouter>
+    </>
   );
 }
 
